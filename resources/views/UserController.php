44281ed7db7cd5cc6812df3f06fd0c0847987bf7
Agujas1993 <82470700+Agujas1993@users.noreply.github.com>
2021-10-19 16:29:51 +0200

    v2

---
+++ resources/views/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = [
+            'Joel',
+            'Ellie',
+            'Tess',
+            'Tommy',
+            'Bill',
+            '<script>alert("Click aquí")</script>'
+        ];
+        $title = 'Usuarios';
+
+        return view('users')->with(compact('users', 'title'));
+    }
+
+    public function create()
+    {
+        return 'Creando nuevo usuario';
+    }
+
+    public function show($id)
+    {
+        return 'Mostrando detalles del usuario: ' . $id;
+    }
+}
